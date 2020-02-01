d166114c0cc7c87c473ccd38c908e56f25d60898
Felix (xq) Queißner <git@mq32.de>
2020-02-01 11:27:49 +0100

    Improved background with background shifting.

--- Code/Systems/Background.lua
+++ Code/Systems/Background.lua
@@ -5,6 +5,9 @@ local Background = Concord.system({Starfield})
 function Background:init()
     local pimage = love.graphics.newImage("Assets/Particles/TestStar.png")
 
+    self.background = love.graphics.newImage("Assets/Images/SpaceBackground.png")
+    self.backgroundShift = 0
+
     self.psystems = { }
 
     for i=1,5 do
@@ -12,10 +15,10 @@ function Background:init()
         local psystem = love.graphics.newParticleSystem(pimage, 1000)
 
         psystem:setParticleLifetime(100, 100) -- Particles live at least 2s and at most 5s.
-        psystem:setEmissionRate(5)
+        psystem:setEmissionRate(10)
         psystem:setEmissionArea("uniform", 0, 1080, 0, false)
         psystem:setSizeVariation(1)
-        psystem:setSizes(0.25)
+        psystem:setSizes(0.15)
         psystem:setLinearAcceleration(0, 0, 0, 0) -- Random movement in all directions.
         psystem:setDirection(math.pi)
         psystem:setSpeed( 50 * i, 50 * i )
@@ -33,6 +36,16 @@ end
 
 function Background:draw()
     local dt = love.timer.getDelta()
+
+    self.backgroundShift = self.backgroundShift - 25 * dt
+    if self.backgroundShift <= -1920 then
+        self.backgroundShift = self.backgroundShift + 1920
+    end
+
+    love.graphics.setColor(1, 1, 1)
+
+    love.graphics.draw(self.background, math.floor(self.backgroundShift), 0)
+    love.graphics.draw(self.background, math.floor(self.backgroundShift + 1920), 0)
 
     love.graphics.setColor(1, 1, 1)
     for i=1,5 do

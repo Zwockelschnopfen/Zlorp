6e4d7030168d76538f6351565dd20f874f878391
Felix (xq) Queißner <git@mq32.de>
2020-02-01 23:50:25 +0100

    Fixes a lot of the animation stuff except for pickup

--- Assets/Anims/animation_sheet_filled.lua
+++ Assets/Anims/animation_sheet_filled.lua
@@ -4,13 +4,13 @@
     numRow= 8,
     numCol= 8,
     anim= {    
-        idle={1, 8, 1},
-        walk={9, 12, 1},
-	junkWalk={17, 20, 1},
-	climb={25, 26, 1},
-	junkClimb={33, 34, 1},
-	junkPickup={41, 44, 1},
-	junkIdle={1, 2, 1},
-	repair={49, 50, 1}
+        idle       = {  1,  8, 1 },
+		junkIdle   = {  9, 16, 1 },
+        walk       = { 17, 24, 1 },
+		junkWalk   = { 17, 24, 1 }, -- to be fixed
+		climb      = { 33, 34, 1 },
+		junkClimb  = { 33, 34, 1 },
+		junkPickup = { 57, 58, 1 },
+		repair     = { 57, 58, 1 }
     }
 }
--- Code/Components/AnimStateMachine.lua
+++ Code/Components/AnimStateMachine.lua
@@ -3,94 +3,26 @@ local asm = Concord.component(
             c.currentState = "idle"
             c.states = {
                 idle = function(anim, vals)
-                    if vals.isMoving then
-                        anim:setActiveAnim("walk")
-                        return "walk"
-                    elseif vals.isClimbing then
-                        anim:setActiveAnim("climb")
-                        return "climb"
-                    elseif vals.isPickingUp then
-                        anim:setActiveAnim("junkPickup")
-                        return "junkPickup"
-                    end
-                end,
-                walk = function(anim, vals)
-                    if not vals.isMoving then
-                        anim:setActiveAnim("idle")
-                        return "idle"
-                    elseif vals.isClimbing then
-                        anim:setActiveAnim("climb")
-                        return "climb"
-                    elseif vals.isPickingUp then
-                        anim:setActiveAnim("junkPickup")
-                        return "junkPickup"
-                    end
-                end,
-                climb = function(anim, vals)
-                    if not vals.isClimbing then
-                        if vals.isMoving then
+                    if vals.isClimbing then
+                        if vals.hasJunk then
+                            anim:setActiveAnim("junkClimb")
+                        else
+                            anim:setActiveAnim("climb")
+                        end
+                    elseif vals.isMoving then
+                        if vals.hasJunk then
+                            anim:setActiveAnim("junkWalk")
+                        else
                             anim:setActiveAnim("walk")
-                            return "walk"
+                        end
+                    else
+                        if vals.hasJunk then
+                            anim:setActiveAnim("junkIdle")
                         else
                             anim:setActiveAnim("idle")
-                            return "idle"
                         end
                     end
                 end,
-                repair = function(anim, vals)
-                        if not vals.isRepairing then
-                            if vals.hasJunk then
-                                anim:setActiveAnim("junkIdle")
-                                return "junkIdle"
-                            else
-                                anim:setActiveAnim("idle")
-                                return "idle"
-                            end
-                        end
-                    end,
-                junkWalk = function(anim, vals)
-                        if not vals.isMoving then
-                            anim:setActiveAnim("junkIdle")
-                            return "junkIdle"
-                        elseif vals.isClimbing then
-                            anim:setActiveAnim("junkClimb")
-                            return "junkClimb"
-                        end
-                    end,
-                junkIdle = function(anim, vals)
-                        if vals.isMoving then
-                            anim:setActiveAnim("junkWalk")
-                            return "junkWalk"
-                        elseif vals.isRepairing then
-                            anim:setActiveAnim("repair")
-                            return "repair"
-                        elseif vals.isClimbing then
-                            anim:setActiveAnim("junkClimb")
-                            return "junkClimb"
-                        end
-                    end,
-                junkClimb = function(anim, vals)
-                        if not vals.isClimbing then
-                            if vals.isMoving then
-                                anim:setActiveAnim("junkWalk")
-                                return "junkWalk"
-                            else
-                                anim:setActiveAnim("junkIdle")
-                                return "junkIdle"
-                            end
-                        end
-                    end,
-                junkPickup = function(anim, vals)
-                        if not vals.isPickingUp then
-                            if vals.hasJunk then
-                                anim:setActiveAnim("junkIdle")
-                                return "junkIdle"
-                            else
-                                anim:setActiveAnim("idle")
-                                return "idle"
-                            end
-                        end
-                    end
             }
             c.transitions = {}
             c.values = {

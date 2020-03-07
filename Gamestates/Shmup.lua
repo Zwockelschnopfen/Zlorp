fe52aceda003e4fcd383b75d88332ede9ed45b9b
Johannes Schultz <git42@sagagames.de>
2020-03-07 19:23:44 +0100

    Komma klar

--- Gamestates/Shmup.lua
+++ Gamestates/Shmup.lua
@@ -48,7 +48,7 @@ function Shmup:load()
         laser = SoundFX("Assets/Sounds/laser", 4),
         laser_weak = SoundFX("Assets/Sounds/laser_weak", 4),
         rocket = SoundFX("Assets/Sounds/rocket", 1),
-        rocket_weak = SoundFX("Assets/Sounds/rocket_weak" 1),
+        rocket_weak = SoundFX("Assets/Sounds/rocket_weak", 1),
         explosion = SoundFX("Assets/Sounds/explosion", 3),
         plasmaShot = SoundFX("Assets/Sounds/plasma", 1),
         plasmaShot_weak = SoundFX("Assets/Sounds/plasma_weak", 1),
